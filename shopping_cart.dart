d07535feb2cb68cbc7d8e9358d221cd3620aad17
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-03-25 19:19:46 -0300

    Adding deletion of items;

--- shopping_cart.dart
+++ shopping_cart.dart
@@ -3,17 +3,30 @@ import 'dart:io';
 main() {
   List<String> products = [];
   bool condition = true;
+
   while (condition) {
     print("=== Add a product ===");
     String text = stdin.readLineSync()!;
+
     if (text == "exit") {
       print("=== ended the program ===");
       condition = false;
     } else if (text == "print") {
-      print(products);
-      print("\n");
+      for (var i = 0; i < products.length; i++) {
+        print("ITEM $i - ${products[i]}");
+      }
+    } else if (text == "delete") {
+      print("=== Which item do you want to delete?");
+      for (var i = 0; i < products.length; i++) {
+        print("ITEM $i - ${products[i]}");
+      }
+      int item = int.parse(stdin.readLineSync()!);
+      products.removeAt(item);
+      print("=== Deleted item");
     } else {
       products.add(text);
+
+      print("\x1B[2J\x1B[0;0H");
     }
   }
 }
